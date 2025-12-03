83483a874b2960e96a4f46a29e1692935f88ebae
gbduek <94713966+gbduek@users.noreply.github.com>
2025-12-03 15:55:50 -0300

    Refactor camera-mqtt.ino for improved MQTT and WiFi handling

--- camera-mqtt.ino
+++ camera-mqtt.ino
@@ -1,105 +1,70 @@
-#include "esp_camera.h"
 #include <WiFi.h>
-#include <PubSubClient.h>
-#include <Base64.h>
+#include <WiFiClientSecure.h>
+#include "certificados.h"
+#include <MQTT.h>
+#include <esp_camera.h>
 
-const char* ssid = "ssid";
-const char* password = "password";
-
-const char* mqtt_server = "test.mosquitto.org";
-const char* topic_photo = "/camera/foto";
-const char* topic_data  = "/camera/dados";
-
-WiFiClient espClient;
-PubSubClient client(espClient);
-
-// CAMERA CONFIGURATION ----------------
+// CAMERA CONFIG
 camera_config_t config = {
-  .pin_pwdn       = 32,
-  .pin_reset      = -1,
-  .pin_xclk       = 0,
-  .pin_sscb_sda   = 26,
-  .pin_sscb_scl   = 27,
-
-  .pin_d7         = 35,
-  .pin_d6         = 34,
-  .pin_d5         = 39,
-  .pin_d4         = 36,
-  .pin_d3         = 21,
-  .pin_d2         = 19,
-  .pin_d1         = 18,
-  .pin_d0         = 5,
-  .pin_vsync      = 25,
-  .pin_href       = 23,
-  .pin_pclk       = 22,
-
-  .xclk_freq_hz   = 20000000,
-  .ledc_timer     = LEDC_TIMER_0,
-  .ledc_channel   = LEDC_CHANNEL_0,
-  .pixel_format   = PIXFORMAT_JPEG,
-  .frame_size     = FRAMESIZE_QVGA,
-  .jpeg_quality   = 10,
-  .fb_count       = 1
+ .pin_pwdn = -1, .pin_reset = -1,
+ .pin_xclk = 15, .pin_sscb_sda = 4, .pin_sscb_scl = 5,
+ .pin_d7 = 16, .pin_d6 = 17, .pin_d5 = 18, .pin_d4 = 12,
+ .pin_d3 = 10, .pin_d2 = 8, .pin_d1 = 9, .pin_d0 = 11,
+ .pin_vsync = 6, .pin_href = 7, .pin_pclk = 13,
+ .xclk_freq_hz = 20000000,
+ .ledc_timer = LEDC_TIMER_0, .ledc_channel = LEDC_CHANNEL_0,
+ .pixel_format = PIXFORMAT_JPEG, .frame_size = FRAMESIZE_SVGA,
+ .jpeg_quality = 10, .fb_count = 2, .grab_mode = CAMERA_GRAB_LATEST
 };
-// --------------------------------------
 
-void setup_wifi() {
-  delay(100);
-  WiFi.begin(ssid, password);
-  
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+WiFiClientSecure conexaoSegura;
+
+void reconectarWiFi() {
+  if (WiFi.status() != WL_CONNECTED) {
+    WiFi.begin("Projeto", "2022-11-07");
+    Serial.print("Conectando ao WiFi...");
+    while (WiFi.status() != WL_CONNECTED) {
+      Serial.print(".");
+      delay(1000);
+    }
+
+    Serial.print("conectado!\nEndereço IP: ");
+    Serial.println(WiFi.localIP());
   }
 }
 
-void reconnect() {
-  while (!client.connected()) {
-    client.connect("ESP32CAM");
+MQTTClient mqtt(1000);  // tamanho máximo das mensagens (1000 bytes)
+void reconectarMQTT() {
+  if (!mqtt.connected()) {
+    Serial.print("Conectando MQTT...");
+    while (!mqtt.connected()) {
+      mqtt.connect("IDENTIFICADOR ÚNICO SEU", "aula", "zowmad-tavQez");
+      Serial.print(".");
+      delay(1000);
+    }
+    Serial.println(" conectado!");
+
+    mqtt.subscribe("topico1");                 // qos = 0
+    mqtt.subscribe("topico2/+/parametro", 1);  // qos = 1
   }
 }
 
-void sendPhoto() {
-  camera_fb_t * fb = esp_camera_fb_get();
-  if (!fb) {
-    return;
-  }
-
-  // Convert JPEG to Base64
-  String imageBase64 = base64::encode(fb->buf, fb->len);
-
-  // Send over MQTT
-  client.publish(topic_photo, imageBase64.c_str());
-
-  // Send metadata
-  client.publish(topic_data, "{\"status\":\"ok\",\"source\":\"esp32cam\"}");
-
-  esp_camera_fb_return(fb);
+void recebeuMensagem(String topico, String conteudo) {
+  Serial.println(topico + ": " + conteudo);
 }
 
 void setup() {
   Serial.begin(115200);
-  Serial1.begin(115200);
-
-  // Start camera
-  esp_camera_init(&config);
-
-  // Connect WiFi
-  setup_wifi();
-
-  client.setServer(mqtt_server, 1883);
-
-  reconnect();
+  delay(500);
+  esp_err_t err = esp_camera_init(&config); 
+  reconectarWiFi();
+  conexaoSegura.setCACert(certificado1);
+  mqtt.begin("mqtt.janks.dev.br", 8883, conexaoSegura);
+  mqtt.onMessage(recebeuMensagem);
+  reconectarMQTT();
 }
-
 void loop() {
-  if (Serial1.available()) {
-    String msg = Serial1.readStringUntil('\n');
-    Serial.println("Recebido: " + msg);
-  }
-
-  if (!client.connected()) reconnect();
-  client.loop();
-
-  sendPhoto();
-  delay(3000); // 3 seconds per photo
+  reconectarWiFi();
+  reconectarMQTT();
+  mqtt.loop();
 }
